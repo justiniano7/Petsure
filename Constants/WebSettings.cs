519f4b365918da6658561c8504e8fc5ace090b37
justin.montalban <justin.montalban@PHEMSSSMW-L0522>
2017-01-26 00:10:14 +0800

    latest

---
+++ Constants/WebSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Constants
+{
+    public class WebSettings
+    {
+        public static string FILE_REPO()
+        {
+            return "";
+            //return ConfigurationManager.AppSettings["FILE_REPO"];
+        }
+    }
+}
